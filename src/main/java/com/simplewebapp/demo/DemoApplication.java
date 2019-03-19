ba5d3130b7aa9f5901e981a0639e31d0f5921fa2
Nompumelelo <lelonompumelelo@gmail.com>
2019-03-19 19:57:36 +0200

    Configured new persistence layer

--- src/main/java/com/simplewebapp/demo/DemoApplication.java
+++ src/main/java/com/simplewebapp/demo/DemoApplication.java
@@ -2,7 +2,12 @@ package com.simplewebapp.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@EnableJpaRepositories("com.simplewebapp.demo.repository")
+@EntityScan("com.simplewebapp.demo.model")
 @SpringBootApplication
 public class DemoApplication {
   public static void main(String[] args) {
